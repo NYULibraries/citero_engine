f0c6fd28a6f33d2f41bb04f63b3dcbc39025b94a
hab278 <as>
2012-10-23 18:07:03 -0400

    Added content type for now, writing new tests

--- app/controllers/citation/cite_controller.rb
+++ app/controllers/citation/cite_controller.rb
@@ -4,7 +4,7 @@ require "citation/engine"
 module Citation
   class CiteController < ApplicationController
     def index
-      send_data( Citation.map(params[:map]).from(params[:from]).to(params[:to]) , :filename => "export."+params[:to])
+      send_data( Citation.map(params[:map]).from(params[:from]).to(params[:to]) , :filename => "export."+params[:to], :type => "text/plain")
     end
   end
 end
