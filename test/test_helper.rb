bd7c98117f0659e4427cc27676f4e95022eabf4f
hab278 <as>
2013-01-16 17:50:34 -0500

    simplecov added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,11 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start
 
 Rails.backtrace_cleaner.remove_silencers!
 
