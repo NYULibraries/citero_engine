47c35a2e0f789baab76efa2a06201f9e92c499f4
hab278 <as>
2013-01-22 14:21:07 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ CiteroEngine::Engine.routes.draw do
   root :to => 'cite#index'
   match "cite/:id" => 'cite#redir'
   match "cite" => 'cite#redir'
-  match "translate/:data/:from/:to" => 'cite#translate'
+  match "translate/:data/:from_format/:to_format" => 'cite#translate'
   post "records" => 'cite#create'
 end
