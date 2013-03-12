6284e149255478fa97c8e1bf537f0c113a12f872
hab278 <as>
2013-03-12 16:37:38 -0400

    Changed to from_format

--- test/dummy/app/models/type_two.rb
+++ test/dummy/app/models/type_two.rb
@@ -1,5 +1,5 @@
 class TypeTwo < ActiveRecord::Base
-  attr_accessible :to_format, :data, :title
+  attr_accessible :from_format, :data, :title
   acts_as_citable do |c|
     c.format_field = 'from_format'
   end
