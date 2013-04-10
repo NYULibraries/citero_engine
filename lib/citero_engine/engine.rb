cabb8d62b90e8b79b3302a2cf83b1aafe8a796bb
hab278 <as>
2013-04-10 13:35:36 -0400

    Reverting back to http calls, and using refworks form

--- lib/citero_engine/engine.rb
+++ lib/citero_engine/engine.rb
@@ -30,7 +30,15 @@ module CiteroEngine
        "enctype" => "application/x-www-form-urlencoded"
        ])
     @endnote ||= PushFormat.new :name => :endnote, :to_format => :ris, :action => :redirect, :url => 'http://www.myendnoteweb.com/?func=directExport&partnerName=Primo&dataIdentifier=1&dataRequestUrl='
-    @refworks ||= PushFormat.new :name => :refworks, :to_format => :ris, :action => :redirect, :url => 'http://www.refworks.com/express/ExpressImport.asp?vendor=Primo&filter=RIS%20Format&encoding=65001&url='
+    # @refworks ||= PushFormat.new :name => :refworks, :to_format => :ris, :action => :redirect, :url => 'http://www.refworks.com/express/ExpressImport.asp?vendor=Primo&filter=RIS%20Format&encoding=65001&url='
+    @refworks ||= PushFormat.new( :name => :refworks, :to_format => :ris, :action => :render, :template => "citero_engine/cite/external_form",
+    :vars => Hash[
+       "elements" => [{:name => 'ImportData', :value => "[ @output ]", :type => 'textarea'}],
+       "name" => "ExportRWForm",
+       "action" => "http://www.refworks.com/express/ExpressImport.asp?vendor=Primo&filter=RefWorks%20Tagged%20Format&encoding=65001",
+       "method" => "POST",
+       "enctype" => "application/x-www-form-urlencoded"
+       ])
     @@push_formats ||= Hash[@easybib.name => @easybib, @endnote.name => @endnote, @refworks.name => @refworks]
   end
 end
