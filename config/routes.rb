4bdd560e879850639e9b58f72fe4562c3433bf2b
hab278 <as>
2013-04-12 15:32:48 -0400

    Added ID optional parameter

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 ExCite::Engine.routes.draw do
-  match "export_citations(/:to_format)" => 'export_citations#index', :via => [:get, :post], :as => "export_citations"
+  match "export_citations(/:to_format)(/:id)" => 'export_citations#index', :via => [:get, :post], :as => "export_citations"
 end
