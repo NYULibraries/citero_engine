afaa70f60ef1a0757f407a6a711c7e1d61cd564a
hab278 <as>
2013-05-14 16:53:51 -0400

    Small bugfixes, but should solve a lot of problems

--- app/helpers/ex_cite/application_helper.rb
+++ app/helpers/ex_cite/application_helper.rb
@@ -1,7 +1,7 @@
 module ExCite
   module ApplicationHelper
     def build_external_form
-      return "<textarea name=\"#{@push_to.name}\" id=\"#{@push_to.name}\">#{@output}</textarea>"
+      return "<textarea name=\"#{@push_to.element_name}\" id=\"#{@push_to.element_name}\">#{@output}</textarea>"
     end
   end
 end
