b2940caa7428f37b962c0c09dc2cf0a08e7571b1
hab278 <as>
2013-05-17 15:36:09 -0400

    Readme update, services update, and coveralls update

--- app/models/ex_cite/push_format.rb
+++ app/models/ex_cite/push_format.rb
@@ -2,13 +2,12 @@ module ExCite
   # Citation class, holds data from format and/or resource key
   class PushFormat
     # Required fields
-    attr_accessor :name, :to_format, :action, :vars, :template, :url, :method, :enctype, :element_name
+    attr_accessor :name, :to_format, :action, :template, :url, :method, :enctype, :element_name
     def initialize args = {}
-      self.name = args[:name]
+      self.name = (args[:name] or 'Service')
       self.to_format = args[:to_format]
-      self.action = args[:action]
-      self.vars = args[:vars]
-      self.template = args[:template]
+      self.action = (args[:action] or :render)
+      self.template = (args[:template] or "ex_cite/cite/external_form")
       self.url = args[:url]
       self.method = (args[:method] or "POST")
       self.enctype = (args[:enctype] or "application/x-www-form-urlencoded")
