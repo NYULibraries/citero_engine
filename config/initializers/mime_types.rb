fd872433e7298cbe3fad1eb28fb6af5502272bee
hab278 <hab278@nyu.edu>
2013-12-09 12:57:48 -0500

    Was registering mime types, better off registering the alias

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,9 +1,9 @@
-Mime::Type.register "application/x-url", :openurl
-Mime::Type.register "text/plain", :csf
-Mime::Type.register "application/json", :easybib
-Mime::Type.register "text/plain", :refworks_tagged
-Mime::Type.register "application/json", :csl
+Mime::Type.register_alias "application/x-url", :openurl
+Mime::Type.register_alias "text/plain", :csf
+Mime::Type.register_alias "application/json", :easybib
+Mime::Type.register_alias "text/plain", :refworks_tagged
+Mime::Type.register_alias "application/json", :csl
 
-Mime::Type.register "text/html", :apa
-Mime::Type.register "text/html", :mla
-Mime::Type.register "text/html", :chicago_author_date
+Mime::Type.register_alias "text/html", :apa
+Mime::Type.register_alias "text/html", :mla
+Mime::Type.register_alias "text/html", :chicago_author_date
