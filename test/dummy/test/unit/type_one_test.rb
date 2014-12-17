be1d70a7fb5e42f974c6775127eaf87bbbb8a221
hab278 <hab278@nyu.edu>
2014-12-17 16:09:30 -0500

    Move tests into the dummy app

--- test/dummy/test/unit/type_one_test.rb
+++ test/dummy/test/unit/type_one_test.rb
@@ -4,4 +4,21 @@ class TypeOneTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "typeones fields should be records fields" do
+    assert_equal "formatting", TypeOne.format_field
+  end
+
+  test "test typeones formatting should populate records fields" do
+    typeone = TypeOne.new(:formatting => "csf", :raw => "itemType: book")
+    assert_equal "csf", typeone.formatting
+  end
+
+  test "test typeones ability to act as citable" do
+    ExCite.acts_as_citable_class = "TypeOne"
+    t = ExCite.acts_as_citable_class.new(:formatting => "csf", :raw => "itemType: book")
+    t.id = 1
+    assert_equal t.to_ris, "TY  - BOOK\nER  -\n\n"
+    ExCite.acts_as_citable_class = "ExCite::Citation"
+  end
 end
--- test/dummy/test/unit/type_two_test.rb
+++ test/dummy/test/unit/type_two_test.rb
@@ -4,4 +4,13 @@ class TypeTwoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "test a typetwos fields should be records fields" do
+    assert_equal "from_format", TypeTwo.format_field
+  end
+
+  test "test typetwos formatting should populate records fields" do
+    typetwo = TypeTwo.new(:from_format => "csf", :data => "itemType: book")
+    assert_equal "csf", typetwo.from_format
+  end
 end
