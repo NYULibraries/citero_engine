1a3a49a9f0b0b4363a6a5d01b525c69de77aeaa0
Eric Griffis <eric.griffis@nyu.edu>
2017-03-17 11:14:13 -0400

    Add basic controller tests (incomplete)

---
+++ spec/controllers/export_citations_controller_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe ExCite::ExportCitationsController, type: :controller do
+  routes { ExCite::Engine.routes }
+
+  describe "GET index" do
+    subject{ response }
+    let(:csf_data){ "itemType: book" }
+    let(:ris_data){ "TY  -  BOOK\nER  -\n\n" }
+    let(:openurl_data){ "https://getit.library.nyu.edu/resolve?rft_val_fmt=info:ofi/fmt:kev:mtx:book" }
+    let(:bibtex_data){ "@book{????\n}" }
+    let(:pnx_data){ "<display><type>book</type></display>" }
+    let(:refworks_data){ "RT Book, whole\nER \n \n" }
+
+    context "with from_format" do
+      context "with data" do
+        before { get :index, to_format: to_format, from_format: from_format, data: data }
+
+        context "from CSF" do
+          let(:from_format){ "csf" }
+          let(:data){ csf_data }
+          include_examples "success for all to_format"
+        end
+
+        context "from BibTeX" do
+          let(:from_format){ "bibtex" }
+          let(:data){ bibtex_data }
+          include_examples "success for all to_format"
+        end
+
+        context "from Refworks" do
+          let(:from_format){ "refworks_tagged" }
+          let(:data){ refworks_data }
+          include_examples "success for all to_format"
+        end
+
+        context "from RIS" do
+          let(:from_format){ "ris" }
+          let(:data){ ris_data }
+          include_examples "success for all to_format"
+        end
+
+        context "from openurl" do
+          let(:from_format){ "openurl" }
+          let(:data){ openurl_data }
+          include_examples "success for all to_format", "openurl"
+        end
+
+        context "from PNX" do
+          let(:from_format){ "pnx" }
+          let(:data){ pnx_data }
+          include_examples "success for all to_format"
+        end
+      end
+
+      context "with id" do
+        before { get :index, to_format: to_format, from_format: from_format, id: id }
+
+        context "invalid" do
+          let(:id) { "error" }
+          let(:to_format){ "pnx" }
+          let(:from_format){ "pnx" }
+          it { is_expected.to be_bad_request }
+        end
+      end
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'rspec-html-matchers'
+require 'support/shared_examples/success_for_all_to_formats'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
