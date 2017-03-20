dcea57b47fc8bff9c49ae34c074d959591b004be
Eric Griffis <eric.griffis@nyu.edu>
2017-03-20 15:54:23 -0400

    Convert status checks to give better error messages

--- spec/support/shared_examples/bad_request_for_all_to_formats.rb
+++ spec/support/shared_examples/bad_request_for_all_to_formats.rb
@@ -1,38 +1,38 @@
 RSpec.shared_examples "bad_request for all to_format" do |from_format|
   context "to openurl" do
     let(:to_format){ "openurl" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to ris" do
     let(:to_format){ "ris" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to refworks_tagged" do
     let(:to_format){ "refworks_tagged" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to refworks" do
     let(:to_format){ "refworks" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to bibtex" do
     let(:to_format){ "bibtex" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to easybib" do
     let(:to_format){ "easybib" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to easybibpush" do
     let(:to_format){ "easybibpush" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to csl" do
     let(:to_format){ "csl" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
   context "to csf" do
     let(:to_format){ "csf" }
-    it { is_expected.to be_bad_request }
+    its(:status) { is_expected.to eq 400 }
   end
 end
--- spec/support/shared_examples/success_for_all_to_formats.rb
+++ spec/support/shared_examples/success_for_all_to_formats.rb
@@ -1,40 +1,40 @@
 RSpec.shared_examples "success for all to_format" do |from_format|
   context "to openurl" do
     let(:to_format){ "openurl" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
   context "to ris" do
     let(:to_format){ "ris" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
   context "to refworks_tagged" do
     let(:to_format){ "refworks_tagged" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
   context "to refworks" do
     let(:to_format){ "refworks" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
     it { is_expected.to render_template "ex_cite/cite/external_form" }
   end
   context "to bibtex" do
     let(:to_format){ "bibtex" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
   context "to easybib" do
     let(:to_format){ "easybib" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
   context "to easybibpush" do
     let(:to_format){ "easybibpush" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
     it { is_expected.to render_template "ex_cite/cite/external_form" }
   end
   context "to csl" do
     let(:to_format){ "csl" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
   context "to csf" do
     let(:to_format){ "csf" }
-    it { is_expected.to be_success }
+    its(:status) { is_expected.to eq 200 }
   end
 end
