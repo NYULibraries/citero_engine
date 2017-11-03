b985f755c4ba398debfc131c00b961d5feff67e2
hab278 <hab278@nyu.edu>
2017-11-03 16:07:52 -0400

    Use constants for HTTP codes

--- spec/support/shared_examples/resource_key_success_for_all_formats.rb
+++ spec/support/shared_examples/resource_key_success_for_all_formats.rb
@@ -35,39 +35,39 @@ RSpec.shared_examples "resource_key-stubbed success for all to_format" do |from_
 
   context "to openurl" do
     let(:to_format){ "openurl" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
   end
   context "to ris" do
     let(:to_format){ "ris" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
   end
   context "to refworks_tagged" do
     let(:to_format){ "refworks_tagged" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
   end
   context "to refworks" do
     let(:to_format){ "refworks" }
     let(:cached_data_converted){ refworks_tagged_data }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
     it { is_expected.to render_template "citero_engine/cite/external_form" }
   end
   context "to bibtex" do
     let(:to_format){ "bibtex" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
   end
   context "to easybib" do
     let(:to_format){ "easybib" }
     let(:cached_data_converted){ "[{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[]}]" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
   end
   context "to easybibpush" do
     let(:to_format){ "easybibpush" }
     let(:cached_data_converted){ "[{\"source\":\"book\",\"book\":{\"title\":null},\"pubtype\":{\"main\":\"pubnonperiodical\"},\"pubnonperiodical\":{},\"contributors\":[]}]" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
     it { is_expected.to render_template "citero_engine/cite/external_form" }
   end
   context "to csf" do
     let(:to_format){ "csf" }
-    its(:status) { is_expected.to eq 200 }
+    its(:status) { is_expected.to eq OK }
   end
 end
